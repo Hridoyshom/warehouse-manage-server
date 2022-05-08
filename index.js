a5af03524a2d22633ee5cb39ad496420d7cf1b97
Hridoyshom <hshom827@gmail.com>
2022-05-08 23:38:15 +0600

    conncetion established

--- index.js
+++ index.js
@@ -10,6 +10,38 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9vuii.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        await client.connect();
+        const itemCollection = client.db('watchWarehouse').collection('item');
+
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running Watch Server');
+});
+
+app.listen(port, () => {
+    console.log('listening to port', port);
+})
+
 
 
 
